1a9cc72b03ebc91fb6c776c9064726c1f01dfe5b
EriOsoy <eosoy@CentroHistorico.local>
2025-09-04 13:55:37 -0600

    feat: se hace responsivo el menu

---
+++ src/pages/layout/Navbar.tsx
@@ -0,0 +1,76 @@
+import { useState } from "react";
+import { FaAlignJustify, FaTimes, FaUser } from "react-icons/fa";
+import { FaChartSimple, FaRightToBracket, FaUserGroup } from "react-icons/fa6";
+import { Link } from "react-router-dom";
+
+export const Navbar = () => {
+  const [isOpen, setIsOpen] = useState<boolean>(false);
+  return (
+    <>
+      <div className="topbar">
+        <button className="hamburger" onClick={() => setIsOpen(!isOpen)}>
+          <FaAlignJustify />
+        </button>
+        <h1 className="title">EGC Admin</h1>
+      </div>
+
+      <aside className={`sidebar ${isOpen ? "open" : "collapsed"}`}>
+        <div className="sidebar-header">
+          <img src="/egc.jpeg" alt="Logo" className="logo" />
+          <h2>EGC Admin</h2>
+        </div>
+
+        <div className="sidebar-content">
+          <hr className="separator" />
+          {isOpen ? (
+            <button className="close-btn" onClick={() => setIsOpen(false)}>
+              <FaTimes /> <span className="text">Ocultar menú</span>
+            </button>
+          ) : (
+            <button className="hamburger" onClick={() => setIsOpen(true)}>
+              <FaAlignJustify className="icon" />
+            </button>
+          )}
+          <hr className="separator" />
+
+          <p>Sistema de Administración</p>
+
+          <div className="menu-continer-instructor">
+            <FaUser className="icon" />
+            <div className="menu-instructor-info">
+              <span>Erick Osoy</span>
+              <p>Administrador</p>
+            </div>
+          </div>
+
+          <nav className="menu-items">
+            <Link
+              to="/dashboard"
+              className="menu-link"
+              onClick={() => setIsOpen(false)}
+            >
+              <FaChartSimple className="icon" />
+              <span className="text">Dashboard</span>
+            </Link>
+            <Link
+              to="/usuarios"
+              className="menu-link"
+              onClick={() => setIsOpen(false)}
+            >
+              <FaUserGroup className="icon" />
+              <span className="text">Usuarios</span>
+            </Link>
+          </nav>
+        </div>
+
+        {/* Logout fijo abajo */}
+        <div className="logout">
+          <button type="button" className="logout-btn">
+            <FaRightToBracket className="icon" />
+            <span className="text">Cerrar sesión</span>
+          </button>
+        </div>
+      </aside>
+    </>
+  );
+};
